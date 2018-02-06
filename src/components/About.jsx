a1e50260214ef4c92001c1fb1229edf6a65c8600
nian liu <nianliu18@gmail.com>
2018-02-06 14:21:13 -0500

    addrandom

---
+++ src/components/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const About = (props) => {
+  return (
+    <div className="container">
+      <div className="about-container">
+        <h1 className="home-title">
+          Welcome and thank you for visiting
+        </h1>
+        <p className="home-p">
+        <img src="https://i.imgur.com/9xK2P5q.jpg?1" className="profilepic"/>
+        I am a Full-Stack Developer with experience in Express/NodeJs, React, Ruby on Rails and PostgresSQL. My journey as a web developer has taught me to embrace all challenges I face because they always end up to be great learning opportunities. I get excited when I see code I dont understand and I believe that the only constant is life and learning. I adapt well in a fast paced environment and I am exceptional with the organizational and time management skills needed when starting and completing a project.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Experience.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Experience = (props) => {
+  return (
+    <div className="main-container">
+      <div className="dev-container">
+      <h1> Web developer </h1>
+      <p>
+        <img src="https://i.imgur.com/LzIboeu.gif" className="dev-icon"/>
+        Ever since I was young I always loved tinkering with the social media platforms such as Xanga where I designed my own HTML and searched for cool music play boxes to embedd on my sites. Since then technology had always excited me and kept me current with tech. My dive into web development didn't fully happen until I met a software engineer in Taiwan. He explained to me how powerful JavaScript and the React library can be and the many doorways software engineering can open up. Thats when I started to immerse myself deeper into coding by playing with flappy bird browser game tutorials, website design classes from code academy and introduction classes on JavaScript through youtube. My hunger to learn grew a step further when  I challenged myself to taking a coding prep course from App Academy which reinforced me with a deeper understanding of JS functions, algorithms, and problem solving capabilities. My journey only took steps further by joining an immersive three month booth camp through General Assembly which taught to work with teams that leaned on each other for help and equipped me with experience and knowledge on building functioning full stack apps today.
+      </p>
+      </div>
+      <div className="verizon-container">
+      <h1> Solutions Specialist -Verizon </h1>
+      <p>
+        <img src="https://i.imgur.com/rRYo4GP.jpg" className="verizon-icon"/>
+        My time at Verizon has been valuable and cultivating. This company has equipped me with the skills to adapt to constantly changing environments, to think quickly and effectively when faced with day to day challanges. Guess that's why they gave us the title Solutions Specialist because we were always on our toes and had to be comfortable solving problems when we see them. This role previously had prepared me well when entering into the tech/development scene because we too face day to day challenges and it takes patience and diligence to overcome hurdles that come our way. What I also valued from this experience was the reinforcing my communication skills from connecting to customers and professionally attending to their needs and concerns. While working in sales or in tech, soft skills are highly needed to successfully needed to complete daily tasks and agendas, this is why Verizon was a valuable spending stone towards me now current role as a web developer.
+      </p>
+      </div>
+    </div>
+  );
+}
+
+export default Experience;
