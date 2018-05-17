a3499185371c7fc3aa454334028d33539ab00e0c
Nian Liu <nianliu@Nians-MBP.fios-router.home>
2018-05-17 11:35:13 -0400

    minor editing and added new project

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ const About = (props) => {
           Welcome and thank you for visiting
         </h1>
         <p className="home-p">
-        <img src="https://i.imgur.com/9xK2P5q.jpg?1" className="profilepic"/>
+        <img src="https://i.imgur.com/9xK2P5q.jpg?1" alt="picture" className="profilepic"/>
         I am a Full-Stack Developer with experience in Express/NodeJs, React, Ruby on Rails and PostgresSQL. My journey as a web developer has taught me to embrace all challenges I face because they always end up to be great learning opportunities. I get excited when I see code I dont understand and I believe that the only constant is life and learning. I adapt well in a fast paced environment and I am exceptional with the organizational and time management skills needed when starting and completing a project.
         </p>
       </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,37 +4,39 @@ import React from 'react';
 const Projects = (props) => {
   return (
     <div className="mainproject-container">
-      <div recipe-container>
+      <div className="item-a">
+        <h1> Inventory Manager </h1>
+        <img className="inventory-photo" src={ require('../images/inventory-image.png')} />
+        <div className="item-a1">
+          An inventory app allowing users to manage/track inventory expenses, visualize live inventory stock, as well as restock items and at the same time manage sales. This project was developed using backend PSQL, with CSS grid and React frontend.
+        </div>
+        <a href="https://github.com/WindCloud2018/inventory-manager"> Click to GitHub </a>
+      </div>
+      <div className="item-b">
         <h1> Recipe App </h1>
-        <p>
-          <img src="https://i.imgur.com/Li5wRAS.png" className="recipe-icon"/>
+        <img src="https://i.imgur.com/Li5wRAS.png" alt="picture" className="recipe-icon"/>
+        <div className="item-b1">
           What inspired me to to create this app was my passion for cooking. I wanted to deepen my understanding in backend Express/NodeJs and PSQL databases and also create something that I would be able to use when I want write down recipes. This app fetches API information from a server called Edamam which is free and has thousands of recipes to choose from. It gives users the ability to see, create, edit and delete recipes within their app.
         <a href="https://git.generalassemb.ly/nianliu18/project-2.git"> Click to GitHub </a>
-        </p>
+        </div>
       </div>
-      <div>
-        <h1>Simple Weather App</h1>
-        <p>
-          <img src="https://i.imgur.com/Ci28sm0.png" className="weather-icon"/>
-          The weather app is a frontend web app designed using React and fetches from a weather API coming from openweathermap.org. My goal was to make this frontend web app as simple as possible and later get together with another fellow developer and UX designer to improve the overal user experience and design of my project.
-        </p>
-        <a href="http://nianssimpleweatherapp.bitballoon.com/"> Click to Weather App </a>
-      </div>
-      <div>
+      <div className="item-c">
         <h1>Home Remedies Blog</h1>
-        <p>
-          <img src="https://i.imgur.com/5JzAqNS.png" className="medicine-icon"/>
+         <img src="https://i.imgur.com/5JzAqNS.png" alt="picture" className="medicine-icon"/>
+        <div className="item-c1">
             Home Remedies Blog was created due to my love for Eastern medicine and home remedies. My father is an herbalist and ever since I was a kid I would remember making arthritis pills and heart health medicine with my old man. This was also an opportunity for me to learn a new language. This app is built completely on Ruby on Rails and I also dove deeper into understanding the language by implementing Devise Authentication where users are able to sign in and post blogs on their experiences in home remedies and herbal medicine.
-        </p>
-        <a href="git@git.generalassemb.ly:nianliu18/project-3.5-homeRemedyBlog.git"> Click to GitHub </a>
+        </div>
+        <a href="https://github.com/nianliu18/HomeRemediesBlog"> Click to GitHub </a>
       </div>
-      <div>
+      <div className="item-d">
         <h1>Dice Poker Browser Game</h1>
-        <p>
-          <img src="https://i.imgur.com/V06mCA9.png" className="dice-icon"/>
-          This was one of my first projects where I was still using JQuery and Vanilla Javascript to make websites and browser games. Dice Poker is a game I play when I am out with friends drinking. Just like poker users roll their dice and whoever gets the highest poker combination wins. I had a lot of fun with this project because it was independent and it really got me thinking logically on the winning conditions of the game and how I can command my browser game to know when the game is over or not.
-        </p>
+        <img src="https://i.imgur.com/V06mCA9.png" alt="picture" className="dice-icon"/>
+        <div className="item-d1">
+          This was one of my first projects where I was still using JQuery and Vanilla Javascript to make websites and browser games. Dice Poker is a game I play when I am out with friends drinking. Just like poker, players roll their dice and whoever gets the highest poker combination wins. I had a lot of fun with this project because it was independent and it really got me thinking logically on the winning conditions of the game and the coding logic behind the interactions I wanted with the users.
+        </div>
         <a href="http://dice-poker-nian.bitballoon.com/"> Click to Game </a>
+        <a href="https://github.com/nianliu18/dice-poker">
+          Click to GitHub </a>
       </div>
     </div>
   );
